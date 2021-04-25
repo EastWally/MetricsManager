e414ec3c2dbfbbbfc2f32b2288abeeb77e1b40bc
EastWally <z.anastacia.a@gmail.com>
2021-04-25 21:04:00 +0300

    Практическое задание к уроку 1

---
+++ MetricsManager/MetricsManager/ValuesHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public class ValuesHolder
+    {
+        public List<Weather> Values;
+
+        public ValuesHolder()
+        {
+            Values = new List<Weather>();
+        }
+    }
+}
